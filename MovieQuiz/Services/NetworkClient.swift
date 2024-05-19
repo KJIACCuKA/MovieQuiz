17e1adaecb6f14b9899e508a907ad54da486744e
Никита Козловский <nikitakozlovskij@MacBook-Pro-Nikita.local>
2024-05-19 19:32:03 +0300

    sprint_07

--- MovieQuiz/Services/NetworkClient.swift
+++ MovieQuiz/Services/NetworkClient.swift
@@ -1,8 +1,13 @@
 import UIKit
 
+protocol NetworkRouting {
+    func fetch(url: URL, handler: @escaping (Result<Data, Error>) -> Void)
+}
+
 // Загрузка данных по URL
 
-struct NetworkClient {
+struct NetworkClient: NetworkRouting {
+    
     private enum NetworkError: Error {
         case codeError
     }
@@ -11,6 +16,7 @@ struct NetworkClient {
         let request = URLRequest(url: url)
         
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            // проверка на наличие ошибки
             if let error = error {
                 handler(.failure(error))
                 return
@@ -26,6 +32,7 @@ struct NetworkClient {
             guard let data = data else { return }
             handler(.success(data))
         }
+        
         task.resume()
     }
 }
